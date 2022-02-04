08ae9224c3c07ff21ad7705a4a5a39a1a6ea5d95
Raviteja7Lanka <94930984+Raviteja7Lanka@users.noreply.github.com>
2022-02-04 00:23:57 -0500

    Improved Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerTableComponent } from './customer-table/customer-table.component';
+import { MyNavbarComponent } from './my-navbar/my-navbar.component';
+import { SupplierTableComponent } from './supplier-table/supplier-table.component';
 import { TabsComponent } from './tabs/tabs.component';
 
 const routes: Routes = [
-  { path: 'tabs', component: TabsComponent }
+  { path: 'tabs', component: TabsComponent },
+  { path: 'my-navbar', component:MyNavbarComponent},
+  { path: 'supplier-table',component:SupplierTableComponent},
+  { path: 'customer-table',component:CustomerTableComponent}
 ];
 
 @NgModule({
